bb131616e255e45c7c4c1578ffe5857451815cd2
brick <powerzieeee@gmail.com>
2020-05-10 20:34:45 +0300

    Created DAL

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace SPWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
