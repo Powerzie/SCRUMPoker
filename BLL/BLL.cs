13f63ff0fe6fadde88dc6654053832c3e604efa3
brick <powerzieeee@gmail.com>
2020-05-10 21:45:32 +0300

    Created BLL

---
+++ BLL/BLL.cs
@@ -0,0 +1,87 @@
+﻿using DAL;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BuisnesLogicLayer
+    {
+        private DataAccesLayer DAL;
+        public BuisnesLogicLayer()
+        {
+            DAL = new DataAccesLayer();
+        }
+
+        #region USER
+        public bool AddNewUser(UserDTO user)
+        {
+            return DAL.AddNewUser(new User()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Password = user.Password,
+                IsUserOnline = user.IsUserOnline
+            });
+        }
+        public UserDTO GetUserById(int id)
+        {
+            User returnedUser = DAL.GetUserById(id);
+            if (returnedUser == null)
+                return null;
+            else
+            {
+                return new UserDTO()
+                {
+                    Id = returnedUser.Id,
+                    UserName = returnedUser.UserName,
+                    IsUserOnline = returnedUser.IsUserOnline,
+                    Password = returnedUser.Password
+                };
+            }
+        }
+        #endregion
+
+        #region ROOMS
+        public bool CreateNewRoom(int ownerID,string roomName)
+        {
+            return DAL.CreateNewRoom(ownerID, roomName);
+        }
+        public bool DeleteRoomById(int id)
+        {
+            return DAL.DeleteRoomById(id);
+        }
+        public List<RoomDTO> GetRoomsByOwnerId(int id)
+        {
+            List<Room> returnedRooms = DAL.GetRoomsByOwnerId(id);
+            if (returnedRooms == null)
+                return null;
+            else
+            {
+                List<RoomDTO> roomsToReturn = new List<RoomDTO>();
+                foreach(var it in returnedRooms)
+                {
+                    roomsToReturn.Add(new RoomDTO()
+                    { Id = it.Id, NameOfRoom = it.NameOfRoom, OwnerId = it.OwnerId, RoomCode = it.RoomCode }
+                    );
+                }
+                return roomsToReturn;
+            }
+        }
+        public UserDTO GetRoomOwner(int roomCode)
+        {
+            User returnedUser = DAL.GetRoomOwner(roomCode);
+            if (returnedUser == null)
+                return null;
+            else
+            return new UserDTO()
+            { Id=returnedUser.Id,IsUserOnline=returnedUser.IsUserOnline,Password=returnedUser.Password,UserName=returnedUser.Password }
+            );
+        }
+    
+        #endregion
+    }
+}
---
+++ BLL/DTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public bool IsUserOnline { get; set; }
+        public string Password { get; set; }
+   
+    }
+    public class RoomDTO
+    {
+        public int Id { get; set; }
+        public int RoomCode { get; set; }
+        public int OwnerId { get; set; }
+        public string NameOfRoom { get; set; }
+
+    }
+
+}
